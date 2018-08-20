68fe7f8c26a0cf1fd8d12792ac045f09b4811723
charishma45 <41935926+charishma45@users.noreply.github.com>
2018-08-20 12:24:34 +0530

    Create Ques19.java

---
+++ Ques19.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ques19
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int i,fact=1;
+		for(i=1;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+}
