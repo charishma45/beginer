64e0ca15de8ed1615a1dd2fc63509d23be8ea2ce
charishma45 <41935926+charishma45@users.noreply.github.com>
2018-08-20 12:36:28 +0530

    Create Ques20.java

---
+++ Ques20.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ques20
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		
+		for(int i=1;i<=5;i++)
+		{
+		      System.out.print(a*i+" ");
+		}
+		
+	}
+}
