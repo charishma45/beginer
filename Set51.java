afd1c107e0ef241d3c839d0abd7fdebd5408a323
charishma45 <41935926+charishma45@users.noreply.github.com>
2018-08-22 12:42:02 +0530

    Create Set51.java

---
+++ Set51.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Set51
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int x=1;
+		while(n>x)
+		{
+		x=x*2;
+		}
+		if(n==x)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
+		// your code goes here
+	
